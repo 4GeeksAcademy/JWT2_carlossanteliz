d06616af07cfa79f015b27bad085e689e419f0db
Charliekofg <charliekofg@hotmail.com>
2026-02-11 02:48:19 +0000

    password encryption working

--- src/api/routes.py
+++ src/api/routes.py
@@ -10,7 +10,7 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
-
+from werkzeug.security import generate_password_hash, check_password_hash
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
@@ -25,3 +25,60 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/login", methods=["POST"])
+def login():
+
+    data=request.get_json()
+    user=User.query.filter_by(email=data ["email".lower().first()])
+
+
+
+
+    # username = request.json.get("username", None)
+    # password = request.json.get("password", None)
+    # if username != "test" or password != "test":
+    #     return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=str(user.id))
+    return jsonify({
+        "token": access_token,
+        "message": "logged in sucessfully",
+        "user": user.serialize()}), 200
+
+@api.route('/user', methods=['POST'])
+def create_user():
+
+    data= request.get_json()
+
+    if not data:
+        return jsonify({"msg": "no se proporcionaron datos"}), 400
+    
+    email= data.get("email")
+    # password=data.get("password") 
+    username=data.get("username")
+
+    existing_user= User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"msg": "ya existe un usuario con ese email"}), 409
+    
+    hashed_password = generate_password_hash(data["password"])
+
+    # //validar que el usuario ya exista ,400
+    # //validar que mande todos los campos ,400
+
+
+    new_user=User(
+        email=email,
+        password=hashed_password,
+        username=username
+    )
+    db.session.add(new_user)
+    
+    try:
+        db.session.commit()
+        return jsonify({"msg": "Usuario creado con éxito"}), 201
+    
+    except Exception as e:
+        print(f"Error al obtener usuarios: {e}")
+        return jsonify({"msg": "Internal Server Error", "error": str(e)}), 500
